b8c567454ba9dff9aaf713bb805eebe6ec04d4b7
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-01-09 13:30:19 +0900

    generates random letters and checks input against them

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,16 @@
 class GamesController < ApplicationController
   def new
     @letters = "lettertest"
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f' ,'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    letterArray = []
+    while letterArray.size < 10
+      newletter = alphabet.sample
+      letterArray.push(newletter)
+    end
+
+    @letters = letterArray.join(" ").upcase
+
   end
 
   def score
@@ -8,9 +18,21 @@ class GamesController < ApplicationController
     @oletters = params[:original_letters]
 
     @result = ""
+    matching = 0
+    @test = []
+    @checkletters = @oletters.split
 
-    if @answer == @oletters
+    @answer.each_char do |letter|
+      if (@checkletters.include?(letter.upcase))
+        @test.push(letter)
+        @checkletters.delete_at(@checkletters.index(letter.upcase))
+      end
+    end
+
+    if @test.size == @answer.size
       @result = "GG you win"
+    elsif @test.size != @answer.size
+      @result = "You cannot build #{@answer} out of these letters."
     else
       @result = "Too bad you lose"
     end
